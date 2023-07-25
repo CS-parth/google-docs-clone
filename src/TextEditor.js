20f8653ffcd4e493447d611e8f89fe9fa461a0d0
CS-parth <parth.vijy@gmail.com>
2023-07-25 19:59:57 +0530

    added load and save req/res from socket.io

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,6 +1,8 @@
-import React, { useCallback, useEffect, useRef } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Quill from "quill"
 import "quill/dist/quill.snow.css"
+import {io} from 'socket.io-client'
+import { useParams } from 'react-router-dom'
 /** There are many reasons that may cause the useEffect to re-run 
  * When Strict Mode is on, React will run one extra development-only setup+cleanup cycle before the first real setup. This is a stress-test that ensures that your cleanup logic “mirrors” your setup logic and that it stops or undoes whatever the setup is doing. If this causes a problem, implement the cleanup function.
  * If some of your dependencies are objects or functions defined inside the component, there is a risk that they will cause the Effect to re-run more often than needed. To fix this, remove unnecessary object and function dependencies. You can also extract state updates and non-reactive logic outside of your Effect.
@@ -12,6 +14,8 @@ import "quill/dist/quill.snow.css"
  * the useeffect is tend to run at least one time 
  * The clearup function will not be called the first time as the its use case is when the next useEffect is being called all the materials that are associated with previous state are cleared
  */
+
+
 
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -31,7 +35,7 @@ export default function TextEditor() {
     //     const editor = document.createElement('div');
     //     wrapperRef.current.append(editor);
     //     // console.log("Effect started");
-    //     new Quill(editor,{theme: "snow"})
+    //     new quill(editor,{theme: "snow"})
 
     //     return ()=>{
     //         // now there is one more  thing taht is if the ref is not defined yet and you started clearingup
@@ -39,6 +43,75 @@ export default function TextEditor() {
     //         wrapperRef.current.innerHTML = "";
     //     }
     // },[])
+    const {id: documentId} = useParams();
+    const [socket,setsocket] = useState(null);
+    const [quill,setQuill] = useState(null);
+    console.log(documentId)
+    useEffect(()=>{ // for recieving the event
+      if(socket == null || quill == null){
+        return
+      }
+      socket.on("recieve-changes", (delta,oldDelta,source) =>{
+          quill.updateContents(delta);
+      })
+
+      return () => {
+        socket.off("recieve-changes", (delta,oldDelta,source) =>{
+          quill.updateContents(delta);
+      })
+      }
+     },[socket,quill])
+
+     useEffect(()=>{
+        if(socket == null || quill == null){ // useEffect will run one time atleast so when the socket or quill is'nt setted they will run in that case we have to return
+          return;
+        }
+        socket.once("load-document", document => {
+          quill.setContents(document);
+          quill.enable();
+        });
+        socket.emit('get-document', documentId);
+     },[socket,quill, documentId])
+
+     useEffect(()=>{
+          if(socket == null || quill == null) return;
+
+          const interval = setInterval(()=>{
+              socket.emit("save-document", quill.getContents());
+          },2000)
+
+          return () => {
+            clearInterval(interval);
+          }
+     },[socket,quill])
+     useEffect(()=>{ // for sending the event // emitting events
+      if(socket == null || quill == null){
+        return;
+      }
+      quill.on("text-change", (delta,oldDelta,source) =>{
+        if(source !== 'user') return
+        socket.emit("send-changes",delta)
+      })
+      
+      return () => {
+        quill.off("text-change", (delta,oldDelta,source) =>{
+          if(source !== 'user') return
+          socket.emit("send-changes",delta)
+        })
+      }
+    },[socket,quill])
+    
+    useEffect(()=>{
+      const s = io("http://localhost:3001");
+      setsocket(s);
+      console.log(s);
+      //optional clearup
+    
+      return () => {
+        console.clear()
+        s.disconnect();
+      }
+    },[])
 
     const wrapperRef = useCallback((wrapper)=>{ // wrapper is wrapperRef.current
         console.log(typeof(wrapper))
@@ -47,8 +120,11 @@ export default function TextEditor() {
         wrapper.innerHTML = "";
         const editor = document.createElement("div");
         wrapper.append(editor);
-        new Quill(editor,{theme: "snow", modules: {toolbar:TOOLBAR_OPTIONS}});
-    },[])
+        const q = new Quill(editor,{theme: "snow", modules: {toolbar:TOOLBAR_OPTIONS}});
+        q.disable(); // until this dont get enabled you wont be able to write in the textfield 
+        q.setText("Loading...");
+        setQuill(q);
+   },[])
 
 
   return (
